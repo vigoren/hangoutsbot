5014d2672097b1ee3826cde543b94af08aa6fe64
Dean Vigoren <vigorator@gmail.com>
2016-08-31 19:38:58 -0600

    Added distance Calculations

--- hangupsbot/plugins/ingress.py
+++ hangupsbot/plugins/ingress.py
@@ -1,4 +1,4 @@
-import hangups, plugins, logging, os, time, pytz, re
+import hangups, plugins, logging, os, time, pytz, re, math
 from datetime import datetime, timedelta
 
 logger = logging.getLogger(__name__)
@@ -6,19 +6,23 @@ logger = logging.getLogger(__name__)
 serverTimeZone = pytz.timezone("UTC")
 hours_per_cycle = 175
 
-def _initialise(bot):  
-    plugins.register_user_command(["allcheckpoints", "allcp", "nextcp", "nextcheckpoint", "nextcycle", "mappin", "getdean"])
+
+def _initialise(bot):
+    plugins.register_user_command(
+        ["allcheckpoints", "allcp", "nextcp", "nextcheckpoint", "nextcycle", "mappin", "getdean", "dist", "distance"])
     plugins.register_admin_command(["settimezone", "addpasscodehangout", "removepasscodehangout", "pc", "whois"])
     os.environ['TZ'] = 'UTC'
     time.tzset()
 
-def _getTimeZone(bot,event):
-    timezone = 'America/Edmonton'    
+
+def _getTimeZone(bot, event):
+    timezone = 'America/Edmonton'
     if bot.memory.exists(["conv_data", event.conv.id_]):
-        if(bot.memory.exists(["conv_data", event.conv.id_, "timezone"])):
+        if (bot.memory.exists(["conv_data", event.conv.id_, "timezone"])):
             timezone = bot.memory.get_by_path(["conv_data", event.conv.id_, "timezone"])
-    
+
     return pytz.timezone(timezone)
+
 
 def settimezone(bot, event, *args):
     """Sets the time zone that should be used for this hangout in calculating all checkpoint times. The time zone name needs to be official from this list https://en.wikipedia.org/wiki/List_of_tz_database_time_zones"""
@@ -29,9 +33,15 @@ def settimezone(bot, event, *args):
     bot.memory.save()
     yield from bot.coro_send_message(event.conv, "The time zone for this hangout has been set to {}".format(timezone))
 
+
+#####################
+# Get Admin stuff
+#####################
+
 def getdean(bot, event, *args):
     """Brings Dean, the bot master, into the chat"""
     yield from bot._client.adduser(event.conv_id, ["117841191535067089864"])
+
 
 #####################
 # Checkpoint stuff
@@ -45,6 +55,7 @@ def _getCycleData():
     cycles = tdelta // cycle
     cycle_start = t0 + (cycles * cycle)
     return {"start": cycle_start, "now": t1}
+
 
 def allcheckpoints(bot, event, *args):
     """Returns a list of every checkpoint for the current cycle. The next checkpoint will be bolded. The checkpoint times are shown for the time zone the hangout is set to."""
@@ -65,9 +76,11 @@ def allcheckpoints(bot, event, *args):
 
     yield from bot.coro_send_message(event.conv, "<br />".join(text))
 
+
 def allcp(bot, event, *args):
     """Returns a list of every checkpoint for the current cycle. The next checkpoint will be bolded. The checkpoint times are shown for the time zone the hangout is set to."""
     yield from allcheckpoints(bot, event, *args)
+
 
 def nextcp(bot, event, *args):
     """Returns the time that the next checkpoint will occur. The checkpoint times are shown for the time zone the hangout is set to."""
@@ -77,11 +90,14 @@ def nextcp(bot, event, *args):
     checkpoint = timedelta(hours=5)
     checkpoints = (cdelta // checkpoint) + 1
     checkpoint_start = data['start'] + (checkpoint * checkpoints)
-    yield from bot.coro_send_message(event.conv, "The next checkpoint is at {:%I:%M%p %Y-%m-%d}".format(checkpoint_start.astimezone(tz)))
+    yield from bot.coro_send_message(event.conv, "The next checkpoint is at {:%I:%M%p %Y-%m-%d}".format(
+        checkpoint_start.astimezone(tz)))
 
-def nextcheckpoint(bot,event,*args):
+
+def nextcheckpoint(bot, event, *args):
     """Returns the time that the next checkpoint will occur. The checkpoint times are shown for the time zone the hangout is set to."""
-    yield from nextcp(bot,event,*args)
+    yield from nextcp(bot, event, *args)
+
 
 def nextcycle(bot, event, *args):
     """Returns the time of the first checkpoint for the next cycle. The checkpoint times are shown for the time zone the hangout is set to."""
@@ -91,11 +107,12 @@ def nextcycle(bot, event, *args):
     text = 'The first checkpoint of the next cycle is at {:%I:%M%p %Y-%m-%d}'.format(start.astimezone(tz))
     yield from bot.coro_send_message(event.conv, text)
 
+
 #####################
 # Maps stuff
 #####################
 
-def mappin(bot,event,*args):
+def mappin(bot, event, *args):
     """
     Takes an Ingress intel link for a portal and creates a Google maps pin to that location
     <b>/ada mappin <url></b>
@@ -107,10 +124,11 @@ def mappin(bot,event,*args):
         return
     res = re.match("-?[1-9]+\.[0-9]+,-?[1-9]+\.[0-9]+", coords[1])
     if not res:
-        yield from bot.coro_send_message(event.conv, "Unable to parse the provided intel link. Make sure it is a link to a portal and try again.")
+        yield from bot.coro_send_message(event.conv,"Unable to parse the provided intel link. Make sure it is a link to a portal and try again.")
         return
 
-    yield from bot.coro_send_message(event.conv, "https://maps.google.com/maps?q="+coords[1])
+    yield from bot.coro_send_message(event.conv, "https://maps.google.com/maps?q=" + coords[1])
+
 
 #####################
 # Passcode stuff
@@ -132,7 +150,8 @@ def addpasscodehangout(bot, event, *args):
     bot.memory.save()
     yield from bot.coro_send_message(event.conv_id, _('The hangout has been added to the list to recieve passcodes.'))
 
-def removepasscodehangout(bot,event,*args):
+
+def removepasscodehangout(bot, event, *args):
     """
     Removes a hangout to the list of hangouts to receive passcodes
     """
@@ -148,9 +167,11 @@ def removepasscodehangout(bot,event,*args):
         hos.remove(oldHo)
         bot.memory["passcode_hangouts"] = hos
         bot.memory.save()
-        yield from bot.coro_send_message(event.conv_id, _('The hangout has been removed from the list to recieve passcodes.'))
+        yield from bot.coro_send_message(event.conv_id,
+                                         _('The hangout has been removed from the list to recieve passcodes.'))
 
-def pc(bot,event,*args):
+
+def pc(bot, event, *args):
     """
     Passes a passcode to the list of passcode hangouts
     """
@@ -161,9 +182,10 @@ def pc(bot,event,*args):
     hos = bot.memory.get("passcode_hangouts")
     for ho in hos:
         yield from bot.coro_send_message(ho, passcodes)
-        logger.info("Sent the pass code '{}' to hangout {}".format(passcodes,ho))
-    
+        logger.info("Sent the pass code '{}' to hangout {}".format(passcodes, ho))
+
     yield from bot.coro_send_message(event.conv_id, _('The passcode(s) have been shared.'))
+
 
 #####################
 # Agent stuff
@@ -184,7 +206,8 @@ def whois(bot, event, *args):
         if "nickname" in user_data:
             curr_nick = user_data['nickname']
         if term in curr_nick.lower() or term in user_data['_hangups']['full_name'].lower():
-            text = "<a href='https://plus.google.com/u/0/{}/about'>{}</a>".format(user_data['_hangups']['gaia_id'], user_data['_hangups']['full_name'])
+            text = "<a href='https://plus.google.com/u/0/{}/about'>{}</a>".format(user_data['_hangups']['gaia_id'],
+                                                                                  user_data['_hangups']['full_name'])
             if curr_nick:
                 text += " aka @{}".format(curr_nick)
             found_users.append(text)
@@ -199,3 +222,120 @@ def whois(bot, event, *args):
 # Distance Stuff
 #####################
 
+def dist(bot, event, *args):
+    """
+    Calculates the distance in KM between 2 portals by using the intel links to those portals. Will also give the minimum requirements to make the link.
+    /ada dist <portal link 1> <portal link 2>
+    """
+    yield from distance(bot, event, *args)
+
+def distance(bot, event, *args):
+    """
+    Calculates the distance in KM between 2 portals by using the intel links to those portals. Will also give the minimum requirements to make the link.
+    /ada distance <portal link 1> <portal link 2>
+    """
+    if not args or len(args) < 2:
+        yield from bot.coro_send_message(event.conv_id,"Not enough portal links were provided. Requires 2 links to calculate the distance.")
+        return
+
+    coord1 = _getCoordsFromUrl(args[0])
+    coord2 = _getCoordsFromUrl(args[1])
+
+    if not coord1 or not coord2:
+        yield from bot.coro_send_message(event.conv_id, "Links provided are not valid intel links.")
+        return
+
+    earthRadius = 6371e3
+    theta1 = math.radians(float(coord1[0]))
+    theta2 = math.radians(float(coord2[0]))
+    delta1 = math.radians(float(coord2[0]) - float(coord1[0]))
+    delta2 = math.radians(float(coord2[1]) - float(coord1[1]))
+
+    a = math.sin(delta1/2) * math.sin(delta1/2) + math.cos(theta1) * math.cos(theta2) * math.sin(delta2/2) * math.sin(delta2/2)
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+    d = earthRadius * c
+    linkReq = _getLinkRequirements(d)
+
+    yield from bot.coro_send_message(event.conv_id, "The portals are <b>{0:.2f} km</b> apart.<br/>{1}".format(d/1000, linkReq))
+
+def _getCoordsFromUrl(url):
+    coords = url.split("pll=")
+    if len(coords) != 2:
+        return False
+    latlng = re.match("-?[1-9]+\.[0-9]+,-?[1-9]+\.[0-9]+", coords[1])
+    if not latlng:
+        return False
+    return coords[1].split(",")
+
+def _getLinkRequirements(distance):
+    minPortalLevel = math.ceil(math.pow(distance/160,1/4))
+    l5Max = 160 * math.pow(5, 4)
+    l6Max = 160 * math.pow(6, 4)
+    l7Max = 160 * math.pow(7, 4)
+    l8Max = 160 * math.pow(8, 4)
+    result = "<br/>Minimum Link Requirements:<br/>"
+
+    if minPortalLevel <= 8:
+        result += "<b>Level {} portal</b> no mods.".format(minPortalLevel)
+
+    if minPortalLevel > 5 and distance <= (l5Max * 8.75):
+        mods = ""
+        ratio = round(distance/l5Max, 2)
+        if ratio <= 2:
+            mods = "1 Rare Link Amp"
+        elif ratio <= 2.5:
+            mods = "2 Rare Link Amps"
+        elif ratio <= 5:
+            mods = "1 Softbank Ultra Link Amp"
+        elif ratio <= 6.25:
+            mods = "2 Softbank Ultra Link Amps"
+        elif ratio <= 7:
+            mods = "1 Very Rare Link Amp"
+        elif ratio <= 8.75:
+            mods = "2 Very Rare Link Amps"
+
+        result += "<br/><b>Level 5 portal</b> with {}.".format(mods)
+
+    if minPortalLevel > 6 and distance <= (l6Max * 10.5):
+        result += _portalMods2People(distance, 6, l6Max)
+    if minPortalLevel > 7 and distance <= (l7Max * 10.5):
+        result += _portalMods2People(distance, 7, l7Max)
+    if minPortalLevel > 8 and distance <= (l8Max * 10.5):
+        result += _portalMods2People(distance, 8, l8Max)
+    if minPortalLevel > 8 and distance > (l8Max * 10.5):
+        result += "Portals are too far apart to link together."
+
+    return result
+
+def _portalMods2People(distance, portalLevel, maxDistance):
+    mods = ""
+    ratio = round(distance / maxDistance, 2)
+    if ratio <= 2:
+        mods = "1 Rare Link Amp"
+    elif ratio <= 2.5:
+        mods = "2 Rare Link Amps"
+    elif ratio <= 2.75:
+        mods = "3 Rare Link Amps"
+    elif ratio <= 3:
+        mods = "4 Rare Link Amps"
+    elif ratio <= 5:
+        mods = "1 Softbank Ultra Link Amp"
+    elif ratio <= 6.25:
+        mods = "2 Softbank Ultra Link Amps"
+    elif ratio <= 6.825:
+        mods = "3 Softbank Ultra Link Amps"
+    elif ratio <= 7:
+        mods = "1 Very Rare Link Amp or 4 Softbank Ultra Link Amps"
+    elif ratio <= 7.5:
+        mods = "4 Softbank Ultra Link Amps"
+    elif ratio <= 8.75:
+        mods = "2 Very Rare Link Amps"
+    elif ratio <= 9.625:
+        mods = "3 Very Rare Link Amps"
+    elif ratio <= 10.5:
+        mods = "4 Very Rare Link Amps"
+
+    if portalLevel > 8:
+        portalLevel = 8
+
+    return "<br/><b>Level {} portal</b> with {}.".format(portalLevel, mods)
